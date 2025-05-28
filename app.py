dece0203c3d847cfa5fd35c4ecf7429fa666f9db
cami3 <43848863+cami3@users.noreply.github.com>
2025-05-28 16:46:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ st.plotly_chart(fig, use_container_width=True)
 
 # --- KPI ---
 st.header("📌 Riepilogo operativo")
-col1, col2, col3, col4, col5, col6 = st.columns(6)
+col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 fatturato = df_sel['fatturato'].sum()
 ingredienti = df_sel['totale_ingredienti'].sum()
 dipendenti = df_sel['Dipendente'].sum()
@@ -268,11 +268,14 @@ col2.metric("Ingredienti", f"€ {ingredienti:,.2f}")
 col3.metric("Dipendenti", f"€ {dipendenti:,.2f}")
 col4.metric("Bibite/Sorbetti", f"€ {bibite_sorbetti:,.2f}")
 col5.metric("Utile stimato", f"€ {utile:,.2f}")
+costi_fissi = 300  # puoi aggiornarlo da una variabile laterale
+utile_netto = fatturato - ingredienti - dipendenti - bibite_sorbetti - costi_fissi
 
-col6 = st.columns(6)[5]
+
+
 ingredienti_grezzi = df_sel['ingredienti_grezzi'].sum()
 col6.metric("Ingredienti Grezzi", f"€ {ingredienti_grezzi:,.2f}")
-
+col7.metric("Utile Netto Stimato", f"€ {utile_netto:,.2f}")
 
 tot_poke = df_sel['poke_totali'].sum()
 tot_extra = df_sel['extra_totali'].sum()
@@ -283,7 +286,7 @@ col3.metric("Costo Medio Ingredienti per Poke", f"€ {ingredienti / tot_poke:.2
 
 
 
-costi_fissi = 300  # puoi aggiornarlo da una variabile laterale
+
 ricavo_medio_poke = fatturato / tot_poke if tot_poke > 0 else 0
 
 if ricavo_medio_poke > 0:
